71842ecfb159958a0e4f3ac4c21db99182dbf25c
acode08 <ajgadi123@gmail.com>
2026-01-24 20:10:36 +0800

    Add domain verification API route

---
+++ app/.well-known/acme-challenge/rENmGzQ439pKiSF_uZkE4SWqnZRq6NY6xShbMxVrKNiKMJkYKpOw9sWfkUIwsThy/route.ts
@@ -0,0 +1,7 @@
+import { NextResponse } from 'next/server';
+
+export async function GET() {
+  return new NextResponse('rENmGzQ439pKiSF_uZkE4SWqnZRq6NY6xShbMxVrKNiKMJkYKpOw9sWfkUIwsThy_M0-EQhbb5eP163A5QsPKBPDqfgsyGnOWpyrEFOnHqug', {
+    headers: { 'Content-Type': 'text/plain' },
+  });
+}
